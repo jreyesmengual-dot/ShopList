daa72e392225e25f66f244462610c0db4fb2c3e9
Alexandre Reyes Mengual <24470180@campeche.tecnm.mx>
2025-11-19 14:31:30 -0600

    Muestra solo la lista de compras

---
+++ ShopList.Gui/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopList.Gui.Models
+{
+    internal class Item
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; } = string.Empty;
+        public int Cantidad { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nombre} ({Cantidad})";
+        }
+    }
+}
---
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -0,0 +1,40 @@
+﻿using ShopList.Gui.Models;
+using System.Collections.ObjectModel;
+
+
+namespace ShopList.Gui.ViewModels
+{
+    public class ShopListViewModel
+    {
+       public ObservableCollection<Item> Items { get; }
+        public ShopListViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+            CargarDatos();
+            
+        }
+        private void CargarDatos()
+        {
+         Items.Add(new Item()
+            {
+                Id = 1,
+                Nombre = "leche",
+                Cantidad = 2,
+            });
+            Items.Add(new Item()
+            {
+                Id = 2,
+                Nombre = "pan bimbo zero",
+                Cantidad = 1,
+            });
+            Items.Add(new Item()
+            {
+                Id = 3,
+                Nombre = "jamon",
+                Cantidad = 500,
+            });
+
+        }
+    }
+    
+}
